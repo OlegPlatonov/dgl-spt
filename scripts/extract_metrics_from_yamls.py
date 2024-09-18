d400bc58047bb4ed4498eaaaf1454233f4cf5ff8
Abusagit <mirotvorez00@gmail.com>
2024-09-18 22:52:29 +0300

    create multiple-run reports [it's more suitable for quota to be scheduled once then to try multiple times]

--- scripts/extract_metrics_from_yamls.py
+++ scripts/extract_metrics_from_yamls.py
@@ -12,51 +12,54 @@ except ImportError:
 experimetal_results_dir: Path = Path(__file__).parent.parent / "experiments"
 
 
-args_file = next(experimetal_results_dir.glob("*/*/args.yaml"))
-metrics_file = next(experimetal_results_dir.glob("*/*/metrics.yaml"))
+results = []
+for exp_dir in experimetal_results_dir.glob("*/*"):
+    try:
+        print(exp_dir)    
+
+        args_file = exp_dir / "args.yaml" 
+        metrics_file = exp_dir / "metrics.yaml"
 
 
-args = yaml.safe_load(open(args_file))
-metrics = yaml.safe_load(open(metrics_file))
+        args = yaml.safe_load(open(args_file))
+        metrics = yaml.safe_load(open(metrics_file))
 
-# print(f"{args_file=}")
-# print(f"{metrics_file=}")
+        print(f"{args_file=}")
+        print(f"{metrics_file=}")
 
-TAG = args["dataset"].replace("-", "_")
+        TAG = args["dataset"].replace("-", "_")
 
-metric_name = args["metric"]
-
-
-metric_field_to_pulsar_unified = {
-    f'val {metric_name} mean': f"val_metric_mean_{TAG}",
-    f'val {metric_name} std': f"val_metric_std_{TAG}",
-    f'test {metric_name} mean': f"test_metric_mean_{TAG}",
-    f'test {metric_name} std': f"test_metric_std_{TAG}",
-}
+        metric_name = args["metric"]
+        
+        args.pop("name")
 
 
-metrics_for_pulsar = []
+        metric_field_to_pulsar_unified = {
+            f'val {metric_name} mean': f"val_metric_mean_{TAG}",
+            f'val {metric_name} std': f"val_metric_std_{TAG}",
+            f'test {metric_name} mean': f"test_metric_mean_{TAG}",
+            f'test {metric_name} std': f"test_metric_std_{TAG}",
+        }
 
-for metric_in_script, metric_for_pulsar_corresponding_name in sorted(metric_field_to_pulsar_unified.items()):
-    metric_value = metrics[metric_in_script]
-    metric_value = metric_value if not np.isnan(metric_value) else -1.0
-    
-    is_main = TAG == "traffic_jams" and metric_for_pulsar_corresponding_name.startswith("test_metric_mean")
-    pulsar_metric_dict = dict(
-        value=metric_value,
-        name=metric_for_pulsar_corresponding_name,
-        tags=[TAG],
-        main=is_main,
-    )
+        for metric_in_script, metric_for_pulsar_corresponding_name in sorted(metric_field_to_pulsar_unified.items()):
+            metric_value = metrics[metric_in_script]
+            metric_value = metric_value if not np.isnan(metric_value) else -1.0
+            
+            pulsar_metric_dict = dict(
+                value=metric_value,
+                name=metric_for_pulsar_corresponding_name,
+                tags=[TAG],
+            )
 
-    metrics_for_pulsar.append(pulsar_metric_dict)
-
-json_string = json.dumps(metrics_for_pulsar)
+            results.append(pulsar_metric_dict)
+    except FileNotFoundError:
+        pass
 
 
+json_string_for_experiment = json.dumps(results)
 
 if ndl:
     with open(ndl.json_output_file(), "w") as f_write:
-        json.dump(metrics_for_pulsar, f_write, indent=4)
+        json.dump(results, f_write, indent=4)
 else:
-    print(json_string)
+    print(results)
